b78b8a66c5e5cfbc061e4e7eef10221e7ffdd5e2
Maciej Zemło <mzemlo.pl@gmail.com>
2020-11-21 10:47:24 +0100

    small

--- main.cpp
+++ main.cpp
@@ -4,11 +4,25 @@
 // SERIAL PINS
 static BufferedSerial dbg ( PA_9 , NC , 115200 ) ;
 
+/* Passing string sandbox*/
+
+void parse_string ( const std::string& ) ;
+
+/* Passing string sandbox*/
+
 // main() runs in its own thread in the OS
 int main ()
 {
     //DBG configuration
     dbg.set_format ( 8 , BufferedSerial::None , 1 ) ;
+
+/* Passing string sandbox*/
+
+
+/* Passing string sandbox*/
+
+
+/***** String merging
 
     std::string string_1 ( "String1 " ) ;
     std::string string_2 ( "string2" ) ;
@@ -26,5 +40,15 @@ int main ()
     dbg.write ( "\r\nstring_1 after push: " , sizeof ( "\r\nstring_1 after push: " ) ) ;
     dbg.write ( &string_1 , sizeof ( string_1 ) ) ;
 
+String merging *****/
+
 }
 
+/* Passing string sandbox*/
+
+void parse_string ( const std::string& s )
+{
+    
+}
+
+/* Passing string sandbox*/
